d5dbb641f7b6f9260759d830b04b66dfc6698b44
Richard Posert <rich@posert.com>
2024-09-05 14:29:27 -0400

    Allow selection of different output

--- flip-particle-y.py
+++ flip-particle-y.py
@@ -4,7 +4,6 @@ import json
 import numpy as np
 import argparse
 from pathlib import Path
-import matplotlib.pyplot as plt
 
 def main(args):
     with open(Path('~/instance-info.json').expanduser(), 'r') as f:
@@ -24,80 +23,87 @@ def main(args):
         "Flipped particles"
     )
     
-    for jid in job_uids:
+    for i, jid in enumerate(job_uids):
+        out_name = args.particle_output_name[i]
         job = project.find_job(jid)
-        particles = job.load_output("particles")
+        particles = job.load_output(out_name)
 
-        mic_uid = particles["location/micrograph_uid"][0]
-        first_mic_particles = particles.query({"location/micrograph_uid": mic_uid})
-        _, mic = project.download_mrc(first_mic_particles["location/micrograph_path"][0])
-        mic = mic[0,:,:]
-        height, width = mic.shape
+        if not args.no_plots:
 
-        fig, ax = plt.subplots(figsize = (10,10))
-        ax.axis(False)
-        ax.imshow(
-            mic,
-            cmap = "Greys_r",
-            origin = "lower",
-            vmin = np.percentile(mic, 5),
-            vmax = np.percentile(mic, 95)
-        )
+            import matplotlib.pyplot as plt
 
-        ax.plot(
-            first_mic_particles["location/center_x_frac"] * width,
-            first_mic_particles["location/center_y_frac"] * height,
-            color = "#2c2c2c",
-            label= "Before flip",
-            linestyle = "",
-            marker = "o",
-            markersize = 10,
-            markeredgecolor = "white"
-        )
+
+            mic_uid = particles["location/micrograph_uid"][0]
+            first_mic_particles = particles.query({"location/micrograph_uid": mic_uid})
+            _, mic = project.download_mrc(first_mic_particles["location/micrograph_path"][0])
+            mic = mic[0,:,:]
+            height, width = mic.shape
+
+            fig, ax = plt.subplots(figsize = (10,10))
+            ax.axis(False)
+            ax.imshow(
+                mic,
+                cmap = "Greys_r",
+                origin = "lower",
+                vmin = np.percentile(mic, 5),
+                vmax = np.percentile(mic, 95)
+            )
+
+            ax.plot(
+                first_mic_particles["location/center_x_frac"] * width,
+                first_mic_particles["location/center_y_frac"] * height,
+                color = "#2c2c2c",
+                label= "Before flip",
+                linestyle = "",
+                marker = "o",
+                markersize = width / 1000,
+                markeredgecolor = "white"
+            )
+
+            ax.plot(
+                first_mic_particles["location/center_x_frac"] * width,
+                (1 - first_mic_particles["location/center_y_frac"]) * height,
+                color = "#FAB06E",
+                label = "After flip",
+                linestyle = "",
+                marker = "P",
+                markersize = width / 1000,
+                markeredgecolor = "white"
+            )
+            ax.legend(loc = "lower left")
 
         particles["location/center_y_frac"] = 1 - particles["location/center_y_frac"]
 
-        ax.plot(
-            first_mic_particles["location/center_x_frac"] * width,
-            (1 - first_mic_particles["location/center_y_frac"]) * height,
-            color = "#FAB06E",
-            label = "After flip",
-            linestyle = "",
-            marker = "P",
-            markersize = 10,
-            markeredgecolor = "white"
-        )
-        ax.legend(loc = "lower left")
-
-        particle_in_name = f"particles_{jid}"
+        particle_src_name = f"particles_{jid}"
         flip_job.add_input(
             type = "particle",
-            name = particle_in_name
+            name = particle_src_name
         )
         flip_job.connect(
-            target_input = particle_in_name,
+            target_input = particle_src_name,
             source_job_uid = jid,
-            source_output = "particles"
+            source_output = out_name
         )
 
         flip_job.add_output(
             type = "particle",
-            name = particle_in_name,
-            title = f"Flipped particles from {jid}",
+            name = particle_src_name,
+            title = f"Flipped particles {jid}",
             slots = ["location"],
-            passthrough = particle_in_name,
+            passthrough = particle_src_name,
             alloc = particles
         )
-        flip_job.save_output(particle_in_name, particles)
+        flip_job.save_output(particle_src_name, particles)
 
-        flip_job.log_plot(fig, f"Before/after flip for {jid}")
+        if not args.no_plots:
+            flip_job.log_plot(fig, f"Before/after flip for {jid}")
     
     flip_job.stop()
 
 
 
 parser = argparse.ArgumentParser(
-    usage="Flip particle coordinates in Y"
+    usage="flip-particle-y.py {PID} {WID} {JID} {...more JIDs}"
 )
 parser.add_argument(
     "project",
@@ -114,10 +120,30 @@ parser.add_argument(
     metavar="job(s)"
 )
 
+parser.add_argument(
+    "--particle-output-name",
+    help = "Name of the particles output. Give as a single value (e.g., particles) or a list of values the same length as the number of jobs, with each job's particle name (e.g., particles imported_particles)",
+    nargs = "+",
+    default = ["particles"]
+)
+
+parser.add_argument(
+    "--no-plots",
+    help = "Do not produce plots",
+    action = "store_true"
+)
+
 if __name__ == "__main__":
     args = parser.parse_args()
     args.project = args.project if "P" in args.project else f"P{args.project}"
     args.workspace = args.workspace if "W" in args.workspace else f"W{args.workspace}"
     args.job = [jid if "J" in jid else f"J{jid}" for jid in args.job]
+
+    print(args)
+
+    if len(args.job) > 1 and len(args.particle_output_name) == 1:
+        args.particle_output_name = args.particle_output_name * len(args.job)
+    else:
+        assert len(args.job) == len(args.particle_output_name), "Give a single particle output name or a list the same length as the jobs"
 
     main(args)
